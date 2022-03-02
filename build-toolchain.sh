30ffeacc300305265f91e0fa80e272dc8fa15dc4
dnnyhan <ais.muzakky@gmail.com>
2022-03-02 11:34:10 +0700

    Build 😈Danny Clang LLVM
    Signed-off-by: dnnyhan <ais.muzakky@gmail.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,35 +1,83 @@
 #!/usr/bin/env bash
 
+# BASIC INFORMATION, COMPULSORY
+LLVM_NAME="😈Danny"
+TG_TOKEN="5171513339:AAFMofFtLRVxsPlGhqjAFA-gjMyQLMfK2ns"
+TG_CHAT_ID="-1001580662294"
+GH_USERNAME="dnnyhan"
+GH_EMAIL="ais.muzakky@gmail.com"
+GH_TOKEN="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"
+GH_PUSH_REPO_URL="github.com/dnnyhan/Danny-Clang"
+
 set -eo pipefail
 
 # Function to show an informational message
-function msg() {
+msg() {
     echo -e "\e[1;32m$@\e[0m"
 }
 
-# Don't touch repo if running on CI
-[ -z "$GH_RUN_ID" ] && repo_flag="--shallow-clone" || repo_flag="--no-update"
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+# Set a directory
+DIR="$(pwd ...)"
+
+# Inlined function to post a message
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
+
+# Build Info
+rel_date="$(date "+%Y%m%d")" # ISO 8601 format
+rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
+builder_commit="$(git rev-parse HEAD)"
+
+# Send a notificaton to TG
+tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
-msg "Building LLVM..."
+msg "$LLVM_NAME: Building LLVM..."
+tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
 ./build-llvm.py \
-	--clang-vendor "Proton" \
+	--clang-vendor "$LLVM_NAME" \
 	--targets "ARM;AArch64;X86" \
-	"$repo_flag" \
+	--shallows-clone \
 	--pgo kernel-defconfig \
 	--lto full
 
+# Check if the final clang binary exists or not.
+[ ! -f install/bin/clang-1* ] && {
+	err "Building LLVM failed ! Kindly check errors !!"
+	tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
+	exit 1
+}
+
 # Build binutils
-msg "Building binutils..."
+msg "$LLVM_NAME: Building binutils..."
+tg_post_msg "<b>$LLVM_NAME: Building Binutils. . .</b>"
 ./build-binutils.py --targets arm aarch64 x86_64
 
 # Remove unused products
 msg "Removing unused products..."
+tg_post_msg "<b>$LLVM_NAME: Removing unused products...</b>"
 rm -fr install/include
 rm -f install/lib/*.a install/lib/*.la
 
 # Strip remaining products
-msg "Stripping remaining products..."
+tg_post_msg "<b>$LLVM_NAME: Stripping remaining products...</b>"
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
 	strip ${f: : -1}
 done
@@ -43,3 +91,34 @@ for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | gre
 	echo "$bin"
 	patchelf --set-rpath '$ORIGIN/../lib' "$bin"
 done
+
+# Release Info
+pushd llvm-project || exit
+llvm_commit="$(git rev-parse HEAD)"
+short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
+popd || exit
+
+llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
+binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
+clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+
+tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+
+# Push to GitHub
+# Update Git repository
+git config --global user.name $GH_USERNAME
+git config --global user.email $GH_EMAIL
+git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
+pushd rel_repo || exit
+rm -fr ./*
+cp -r ../install/* .
+git checkout README.md # keep this as it's not part of the toolchain itself
+git add .
+git commit -asm "$LLVM_NAME: Bump to $rel_date build
+LLVM commit: $llvm_commit_url
+Clang Version: $clang_version
+Binutils version: $binutils_ver
+Builder commit: https://$GH_PUSH_REPO_URL/commit/$builder_commit"
+git push -f
+popd || exit
+tg_post_msg "<b>$LLVM_NAME: Toolchain pushed to <code>https://$GH_PUSH_REPO_URL</code></b>"
